5c56da583696d13b23299b931f615559addf0767
Rakesh <rgrakesh519@gmail.com>
2025-03-04 02:07:54 +0530

    db change

--- config/db.js
+++ config/db.js
@@ -7,16 +7,24 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, 
     dialect: 'mysql',
     logging: false,
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
-      idle: 10000
+      idle: 100000
+    },
+    dialectOptions: {
+      ssl: {
+        require: true, 
+        rejectUnauthorized: false
+      }
     }
   }
 );
+
 
 // Test connection
 const testConnection = async () => {
